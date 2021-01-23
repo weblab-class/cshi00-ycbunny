b4d0f004edd192be058e6154f73a9dbdf09a5989
Shi <Shi@lindandeMacBook-Pro.local>
2021-01-24 02:52:03 +0800

    deleteComplete

--- client/src/components/modules/GraphingPanel.js
+++ client/src/components/modules/GraphingPanel.js
@@ -11,6 +11,7 @@ const math = create(all)
       this.state = { board: null,  initialGraphingFinished: false}
       this.defaultStyle = { width: 500, height: 500 }
       this.defauflboardAttributes = { axis: true, boundingbox: [-12, 10, 12, -10] }
+      this.curveDic = {}
     };
   
     componentDidMount() {
@@ -29,13 +30,30 @@ const math = create(all)
       if (this.state.board !== null && this.props.functions.length>0 && this.state.initialGraphingFinished ===false) {
         this.state.board.suspendUpdate();
         this.props.functions.map((functionObj) => (
-        this.state.board.create('curve', [function(t){return t;},
-        function(t){return math.evaluate(functionObj.exp,{x:t});}, Number(functionObj.leftRange), Number(functionObj.rightRange)], { strokeColor: '#aa2233', strokeWidth: 3 })
+        this.curveDic[functionObj._id] = this.state.board.create('curve', [function(t){return t;},
+          function(t){return math.evaluate(functionObj.exp,{x:t});}, Number(functionObj.leftRange), Number(functionObj.rightRange)], { strokeColor: '#aa2233', strokeWidth: 3 })
         ));
         this.state.board.unsuspendUpdate();
         this.setState({
           initialGraphingFinished:true
         })
+      }
+      if (this.state.initialGraphingFinished ===true){
+        let currentCurves = Object.keys(this.curveDic);
+        let newCurves = this.props.functions.map((functionObj) => (
+          functionObj._id
+          ));
+        let newCurve = this.props.functions.filter((functionObj) => (this.curveDic[functionObj._id]==null));
+        console.log(newCurve)
+        let remove = currentCurves.filter(f => !newCurves.includes(f))
+        if (newCurve !== null && newCurve.length !== 0){
+          this.curveDic[newCurve[0]._id] = this.state.board.create('curve', [function(t){return t;},
+          function(t){return math.evaluate(newCurve[0].exp,{x:t});}, Number(newCurve[0].leftRange), Number(newCurve[0].rightRange)], { strokeColor: '#aa2233', strokeWidth: 3 });
+        }
+        else if (remove !== null && remove.length !== 0) {
+          this.state.board.removeObject(this.curveDic[remove[0]]);
+          delete this.curveDic[remove[0]];
+        }
       }
       return (
         <>
